edc0973ace2971913aa436d2246d5bf1ed84a02b
Vijatesh Kadimi <vijateshkadimi001@gmail.com>
2026-07-01 09:48:05 +0530

    vijatesh changes
    vijatesh changes

--- frontend/src/components/Menu.jsx
+++ frontend/src/components/Menu.jsx
@@ -1,98 +1,100 @@
 import FoodCard from "./FoodCard";
 
+import vegBiryani from "../assets/veg biryani.jpg";
+import chickenBiryani from "../assets/chicken biryani.jpg";
+import paneerButterMasala from "../assets/panner butter masala.jpg";
+import paneerTikka from "../assets/panner tikka.jpg";
+import butterNaan from "../assets/butter-naan.jpg";
+import bbqChickenWings from "../assets/bbq-chicken-wings.jpg";
+import vegetableSpringRolls from "../assets/Vegetable-Spring-Rolls-.jpeg";
+import iceCream from "../assets/ice cream.jpg";
+import mangoShake from "../assets/mango shake.jpg";
+import cocaCola from "../assets/coca cola.jpg";
+import freshLimeSoda from "../assets/fresh lime soda.jpg";
+import gulabJamun from "../assets/gulab-jamun.jpg";
+
+
 function Menu() {
   const foods = [
     {
-      image:
-        "https://images.unsplash.com/photo-1563379091339-03246963d96c?w=800",
+      image: vegBiryani,
       name: "Veg Biryani",
       description:
         "Fragrant basmati rice cooked with fresh vegetables and aromatic spices.",
       price: 280,
     },
     {
-      image:
-        "https://images.unsplash.com/photo-1563379091339-03246963d96c?w=800",
+      image: chickenBiryani,
       name: "Chicken Biryani",
       description:
         "Authentic Hyderabadi chicken dum biryani served with raita.",
       price: 350,
     },
     {
-      image:
-        "https://images.unsplash.com/photo-1603360946369-dc9bb6258143?w=800",
+      image: paneerButterMasala,
       name: "Paneer Butter Masala",
       description:
         "Soft paneer cubes cooked in a rich and creamy tomato gravy.",
       price: 290,
     },
     {
-      image:
-        "https://images.unsplash.com/photo-1627308595229-7830a5c91f9f?w=800",
+      image: paneerTikka,
       name: "Paneer Tikka",
       description:
         "Grilled paneer cubes marinated with Indian spices.",
       price: 250,
     },
     {
-      image:
-        "https://images.unsplash.com/photo-1601050690597-df0568f70950?w=800",
+      image: vegetableSpringRolls,
       name: "Veg Spring Rolls",
       description:
         "Crispy spring rolls stuffed with fresh vegetables.",
       price: 180,
     },
     {
-      image:
-        "https://images.unsplash.com/photo-1527477396000-e27163b481c2?w=800",
+      image: bbqChickenWings,
       name: "Chicken Wings",
       description:
         "Juicy chicken wings seasoned with flavorful spices.",
       price: 320,
     },
     {
-      image:
-        "https://images.unsplash.com/photo-1603894584373-5ac82b2ae398?w=800",
+      image: butterNaan,
       name: "Butter Naan",
       description:
         "Soft tandoor-baked naan brushed with butter.",
       price: 50,
     },
     {
-      image:
-        "https://images.unsplash.com/photo-1605197161470-5e6dcb8dfe6f?w=800",
+      image: gulabJamun,
       name: "Gulab Jamun",
       description:
         "Soft milk dumplings soaked in delicious sugar syrup.",
       price: 120,
     },
     {
-      image:
-        "https://images.unsplash.com/photo-1563805042-7684c019e1cb?w=800",
+      image: iceCream,
       name: "Ice Cream",
       description:
         "Refreshing ice cream served chilled.",
       price: 150,
     },
     {
-      image:
-        "https://images.unsplash.com/photo-1554866585-cd94860890b7?w=800",
+      image: cocaCola,
       name: "Coca-Cola",
       description:
         "Classic chilled soft drink.",
       price: 60,
     },
     {
-      image:
-        "https://images.unsplash.com/photo-1544145945-f90425340c7e?w=800",
+      image: freshLimeSoda,
       name: "Fresh Lime Soda",
       description:
         "Freshly prepared lime soda served chilled.",
       price: 90,
     },
     {
-      image:
-        "https://images.unsplash.com/photo-1623065422902-30a2d299bbe4?w=800",
+      image: mangoShake,
       name: "Mango Shake",
       description:
         "Creamy mango milkshake made with fresh mangoes.",
